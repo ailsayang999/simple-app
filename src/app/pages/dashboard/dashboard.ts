c7f819c8ec4ce8d5d7fbe8f9d4250a93a75f2400
AilsaYang <ailsayang831031@gmail.com>
2025-11-21 13:43:34 +0800

    [Add] 加入treetable

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TreeNode } from 'primeng/api';
+import { TreeTableModule } from 'primeng/treetable';
+import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
+
+interface Column {
+  field: string;
+  header: string;
+}
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, TreeTableModule, CardModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
+  files!: TreeNode[];
+  cols!: Column[];
 
+  ngOnInit() {
+    this.files = [];
+    for (let i = 0; i < 50; i++) {
+      let node = {
+        data: {
+          name: 'Item ' + i,
+          size: Math.floor(Math.random() * 1000) + 1 + 'kb',
+          type: 'Type ' + i,
+        },
+        children: [
+          {
+            data: {
+              name: 'Item ' + i + ' - 0',
+              size: Math.floor(Math.random() * 1000) + 1 + 'kb',
+              type: 'Type ' + i,
+            },
+          },
+        ],
+      };
+
+      this.files.push(node);
+    }
+
+    this.cols = [
+      { field: 'name', header: 'Name' },
+      { field: 'size', header: 'Size' },
+      { field: 'type', header: 'Type' },
+    ];
+  }
 }
